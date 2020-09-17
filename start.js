329021827bcb162deb2fcca11cb0c8a9b98e745e
Mathias Beckius <lillmatte@hotmail.com>
2020-09-17 10:50:57 +0200

    deploy test

--- start.js
+++ start.js
@@ -2,7 +2,6 @@ const app = require('./app.js');
 const port = process.env.PORT;
 const db = require('./database/db');
 db.connect().then( () => 
-	app.listen( process.env.PORT || 5000, () => console.log('It\'s running birch'))
+    app.listen(port, () => console.log(`Running app on port ${port}`));
 );
 // run server
-app.listen(port, () => console.log(`Running app on port ${port}`));
