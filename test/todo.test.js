a0c0d1e684b4aeaa49e284bab1daed63711b2bdf
Mathias Beckius <lillmatte@hotmail.com>
2020-09-17 10:17:22 +0200

    testar lite

--- test/todo.test.js
+++ test/todo.test.js
@@ -92,4 +92,8 @@ describe('todo model tests', () => {
 		deletedList.list.should.equal(1);
 		deletedList.items.should.equal(list.items.length);
 	});
+	it('test', () => {
+		let test = true;
+		test.should.equal(true);
+	});
 });
