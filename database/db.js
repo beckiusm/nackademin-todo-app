6f942bd7f8faa5ffbfc4e6da79cd63c4b4d49868
Mathias Beckius <lillmatte@hotmail.com>
2020-09-17 14:13:44 +0200

    change db uri

--- database/db.js
+++ database/db.js
@@ -13,13 +13,13 @@ case 'test':
 case 'production':
 	mongoDatabase = {
 		getUri: async () => 
-			`${db}todo?retryWrites=true&w=majority`
+			`${db}`
 	};
 	break;
 case 'staging':
 	mongoDatabase = {
 		getUri: async () => 
-			`${db}todoStage?retryWrites=true&w=majority`
+			`${db}`
 	};
 	break;
 }
